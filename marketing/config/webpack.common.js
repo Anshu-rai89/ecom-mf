bff0a3b6ed1a8257e28606a4df60b20052c0503f
Anshu Rai <raianshu8991@gmail.com>
2023-09-14 22:00:18 +0530

    Adding landing and pricing page of marketing

--- marketing/config/webpack.common.js
+++ marketing/config/webpack.common.js
@@ -1,16 +1,17 @@
 module.exports = {
-    module: 'development',
+  module: {
     rules: [
-        {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-react', '@babel/preset-env'],
-                    plugins: ['@babel/plugin-transform-runtime']
-                }
-            }
-        }
-    ]
-}
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
