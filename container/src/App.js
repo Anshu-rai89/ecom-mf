edfa42da9fa22677b6a1b1d0c89dfa4ec2614d06
Anshu Rai <raianshu8991@gmail.com>
2023-09-15 21:35:23 +0530

    Fixed conflicting classname

--- container/src/App.js
+++ container/src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
 import {BrowserRouter} from 'react-router-dom';
 import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
     return (
       <BrowserRouter>
-        <div>
-          <Header />
-          <hr />
-          <MarketingApp />
-        </div>
+        <StylesProvider generateClassName={generateClassName}>
+          <div>
+            <Header />
+            <hr />
+            <MarketingApp />
+          </div>
+        </StylesProvider>
       </BrowserRouter>
     );
 }
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
-import {StylesProvider} from '@material-ui/core/styles'
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
 import Pricing from './components/Pricing';
 import Landing from './components/Landing';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'mss'
+});
+
 
 export default () => {
     return (
       <div>
         <StylesProvider>
-          <BrowserRouter>
+          <BrowserRouter generateClassName={generateClassName}>
             <Switch>
               <Route path="/pricing" exact component={Pricing} />
               <Route path="/" exact component={Landing} />
